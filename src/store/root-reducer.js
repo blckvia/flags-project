708c6dadaf648b3e36f473a13437ffa9f43c19f3
blckvia <a.vyatkinn@icloud.com>
2023-01-12 13:35:54 +0500

    added some features

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,3 +1,8 @@
 import { combineReducers } from 'redux';
+import { countriesReducer } from './countries/countries-reducer';
+import { themeReducer } from './theme/theme-reducer';
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+    theme: themeReducer,
+    countries: countriesReducer,
+});
