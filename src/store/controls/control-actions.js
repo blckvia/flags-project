05c4c02ab142b48b72e735f3b33658ad263654fb
blckvia <a.vyatkinn@icloud.com>
2023-01-13 12:38:09 +0500

    filtering by region added

--- src/store/controls/control-actions.js
+++ src/store/controls/control-actions.js
@@ -1,8 +1,14 @@
 export const SET_SEARCH = '@@conntrols/SET_SEARCH';
+export const SET_REGION = '@@controls/SET_REGION';
 
 export const setSearch = (search) => ({
     type: SET_SEARCH,
     payload: search,
+});
+
+export const setRegion = (region) => ({
+    type: SET_REGION,
+    payload: region,
 });
 
 // export const search =
--- src/store/countries/countries-selectors.js
+++ src/store/countries/countries-selectors.js
@@ -6,8 +6,10 @@ export const selectCountriesInfo = (state) => ({
 
 export const selectAllCountries = (state) => state.countries.list;
 
-export const selectVidibleCountries = (state, { search = '' }) => {
-    return state.countries.list.filter((country) =>
-        country.name.toLowerCase().includes(search.toLowerCase())
+export const selectVidibleCountries = (state, { search = '', region = '' }) => {
+    return state.countries.list.filter(
+        (country) =>
+            country.name.toLowerCase().includes(search.toLowerCase()) &&
+            country.region.includes(region)
     );
 };
